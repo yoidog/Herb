de9d3299d74c434a0838f2d979bb7740919234f8
Waled Ali <68444929+yoidog@users.noreply.github.com>
2022-07-18 16:38:14 +0300

    Add files via upload

---
+++ src/herbLang/herb/TokenType.java
@@ -0,0 +1,22 @@
+package herbLang.herb;
+
+enum TokenType {
+	// Single character tokens.
+	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+	
+	// One or two character tokens.
+	BANG, BANG_EQUAL,
+	EQUAL, EQUAL_EQUAL,
+	GREATER, GREATER_EQUAL,
+	LESS, LESS_EQUAL,
+	
+	// Literals.
+	IDENTIFIER, STRING, NUMBER,
+	
+	// Keywords.
+	AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NUL, OR,
+	WRITE, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+	
+	EOF
+}
