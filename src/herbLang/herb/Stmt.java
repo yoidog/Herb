b0c963e7538434d5d63871630b1eb02d0d21185c
Waled Ali <68444929+yoidog@users.noreply.github.com>
2022-07-23 00:54:01 +0300

    Update Stmt.java

--- src/herbLang/herb/Stmt.java
+++ src/herbLang/herb/Stmt.java
@@ -1,11 +1,25 @@
-package herbLang.lang;
+package herbLang.herb;
 
 import java.util.List;
 
 abstract class Stmt {
   interface Visitor<R> {
+    R visitBlockStmt(Block stmt);
     R visitExpressionStmt(Expression stmt);
     R visitWriteStmt(Write stmt);
+    R visitVarStmt(Var stmt);
+  }
+  static class Block extends Stmt {
+    Block(List<Stmt> statements) {
+      this.statements = statements;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBlockStmt(this);
+    }
+
+    final List<Stmt> statements;
   }
   static class Expression extends Stmt {
     Expression(Expr expression) {
@@ -31,6 +45,20 @@ abstract class Stmt {
 
     final Expr expression;
   }
+  static class Var extends Stmt {
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
 
- abstract <R> R accept(Visitor<R> visitor);
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+
+    final Token name;
+    final Expr initializer;
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
