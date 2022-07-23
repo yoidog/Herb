b4fe7e9b0668c14c13dac73df414cf904f265838
Waled Ali <68444929+yoidog@users.noreply.github.com>
2022-07-23 12:51:52 +0300

    Update Parser.java

--- src/herbLang/herb/Parser.java
+++ src/herbLang/herb/Parser.java
@@ -1,6 +1,7 @@
 package herbLang.herb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static herbLang.herb.TokenType.*;
@@ -36,12 +37,54 @@ class Parser {
 		}
 	}
 	private Stmt statement() {
+		if (match(FOR)) return forStatement();
 		if (match(IF)) return ifStatement();
 		if (match(WRITE)) return writeStatement();
 		if (match(WHILE)) return whileStatement();
 		if (match(LEFT_BRACE)) return new Stmt.Block(block());
 		
 		return expressionStatement();
+	}
+	private Stmt forStatement() {
+		consume(LEFT_PAREN, "Expected '(' after statement `for`.");
+		
+		Stmt initializer;
+		if (match(SEMICOLON)) {
+			initializer = null;
+		} else if (match(VAR)) {
+			initializer = varDeclaration();
+		} else {
+			initializer = expressionStatement();
+		}
+		
+		Expr condition = null;
+		if (!check(SEMICOLON)) {
+			condition = expression();
+		}
+		consume(SEMICOLON, "Expected ';' after loop condition.");
+		
+		Expr increment = null;
+		if (!check(RIGHT_PAREN)) {
+			increment = expression();
+		}
+		consume(RIGHT_PAREN, "Expect ')' after statement for clauses.");
+		Stmt body = statement();
+		
+		if (increment != null) {
+			body = new Stmt.Block(
+				Arrays.asList(
+					body,
+					new Stmt.Expression(increment)));
+		}
+		
+		if (condition == null) condition = new Expr.Literal(true);
+		body = new Stmt.While(condition, body);
+		
+		if (initializer != null) {
+			body = new Stmt.Block(Arrays.asList(initializer, body));
+		}
+		
+		return body;
 	}
 	private Stmt ifStatement() {
 		consume(LEFT_PAREN, "Expected '(' after statement 'if'.");
@@ -73,13 +116,13 @@ class Parser {
 		return new Stmt.Var(name, initializer);
 	}
 	private Stmt whileStatement() {
-		consume(LEFT_PAREN, "Expected '(' after statement 'while'.");
-		Expr condition = expression();
-		consume(RIGHT_PAREN, "Expected ')' after statement 'while' condition.");
-		Stmt body = statement();
-		
-		return new Stmt.While(condition, body);
-	}
+        consume(LEFT_PAREN, "Expected '(' after statement 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after statement 'while' condition.");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
+    }
 	private Stmt expressionStatement() {
 		Expr expr = expression();
 		consume(SEMICOLON, "Expected ';' / semicolon / end of code symbol after expression.");
@@ -92,7 +135,7 @@ class Parser {
 	      statements.add(declaration());
 	    }
 
-	    consume(RIGHT_BRACE, "Expect '}' after block.");
+	    consume(RIGHT_BRACE, "Expected '}' after block.");
 	    return statements;
 	  }
 	private Expr assignment() {
