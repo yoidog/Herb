034c4c029a6abb2aaff9ca4642524a9f2b152b55
Waled Ali <68444929+yoidog@users.noreply.github.com>
2022-07-24 02:59:24 +0300

    Added `return`.

--- src/herbLang/herb/HerbFunction.java
+++ src/herbLang/herb/HerbFunction.java
@@ -22,7 +22,11 @@ class HerbFunction implements HerbCallable {
 			environment.define(declaration.params.get(i).lexeme,arguments.get(i));
 		}
 		
-		interpreter.executeBlock(declaration.body, environment);
+		try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
 		return null;
 	}
 }
